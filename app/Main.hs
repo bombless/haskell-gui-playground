125a2f78708fc5120ce25ff3a79261ddf6e60368
York Xiang <bombless@126.com>
2024-12-07 14:22:35 +0800

    Basic tree drawing

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import SDL
 import SDL.Font
+import Tree (drawTree)
 
 import Control.Monad (unless)
 
@@ -11,31 +12,27 @@ main = do
   initializeAll
   window <- createWindow "My SDL Application" defaultWindow
   SDL.Font.initialize
-  font <- load "c:\\Windows\\Fonts\\YuGothB.ttc" 300
+  font <- load "c:\\Windows\\Fonts\\YuGothB.ttc" 16
   renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer font
+  appLoop True renderer font
   destroyWindow window
 
-appLoop :: Renderer -> Font -> IO ()
-appLoop renderer font = do
+appLoop :: Bool -> Renderer -> Font -> IO ()
+appLoop firstTime renderer font = do
   events <- pollEvents
-  let eventIsQPress event =
+  let eventIsQuit event =
         case eventPayload event of
+          QuitEvent -> True
           KeyboardEvent keyboardEvent ->
             keyboardEventKeyMotion keyboardEvent == Pressed &&
             keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
           _ -> False
-      qPressed = any eventIsQPress events
+      quitTriggered = any eventIsQuit events
   rendererDrawColor renderer $= V4 0 0 255 255
   clear renderer
   rendererDrawColor renderer $= V4 255 0 0 255
-  let rectFill = Rectangle (P (V2 50 50)) (V2 300 200)
-  fillRectF renderer rectFill
   let fontColor = V4 0 255 0 255
-  surface <- solid font fontColor "hello"
-  (V2 intWidth intHeight) <- surfaceDimensions surface
-  texture <- createTextureFromSurface renderer surface
-  let rectText = Rectangle (P (V2 50 50)) (V2 (div intWidth 8) (div intHeight 8))
-  copy renderer texture Nothing $ Just rectText
+  let drawText = solid font fontColor
+  drawTree firstTime drawText renderer
   present renderer
-  unless qPressed (appLoop renderer font)
+  unless quitTriggered (appLoop False renderer font)
--- app/Tree.hs
+++ app/Tree.hs
@@ -1,4 +1,10 @@
-module Tree where
+module Tree (drawTree) where
+
+import SDL
+import Data.Text (pack, Text)
+import Text.Printf (printf)
+
+import Control.Monad (when)
 
 data Tree a = Leaf | Node a (Tree a) (Tree a)
 data Element a = VirtualLeft | VirtualRight | VirtualNode | VisibleNode a Bool Bool | VisibleLeft | VisibleRight
@@ -97,6 +103,9 @@ class Printable a where
 instance Printable Char where
     printNode a = putChar '|' >> putChar a >> putChar '|'
 
+instance Printable Int where
+    printNode a = printf "%03d" a
+
 printLine :: (Printable a) => [(Int, Element a)] -> IO ()
 printLine [] = putChar '\n'
 printLine ((n, VisibleNode c _ _):t) = do
@@ -121,3 +130,60 @@ printHelper (h:t) = printLine h >> printHelper t
 
 printTree :: (Printable a) => Tree a -> IO ()
 printTree = printHelper . getLines
+
+drawNodes :: Bool -> [[(Int, Element Int)]] -> Int -> Int -> (Int, Int) -> (Text -> IO Surface) -> Renderer -> IO ()
+drawNodes _ [] _ _ _ _ _ = return ()
+drawNodes firstTime ([]:t) _ y (unitWidth, unitHeight) drawText renderer = drawNodes firstTime t 0 (y + unitHeight) (unitWidth, unitHeight) drawText renderer
+drawNodes firstTime (((n, VisibleNode content _ _):t):otherLines) x y (unitWidth, unitHeight) drawText renderer = do
+    surface <- drawText $ pack $ printf "%03d" content
+    texture <- createTextureFromSurface renderer surface
+    dims <- surfaceDimensions surface
+    let cX = fromIntegral $ x + n * unitWidth
+    let cY = fromIntegral y
+    let rectText = Rectangle (P (V2 cX cY)) dims
+    copy renderer texture Nothing $ Just rectText
+    drawNodes firstTime (t:otherLines) (x + n * unitWidth + unitWidth) y (unitWidth, unitHeight) drawText renderer
+drawNodes firstTime (((n, VisibleLeft):t):otherLines) x y (unitWidth, unitHeight) drawText renderer = do
+    let p1x = fromIntegral $ n * unitWidth + x + unitWidth
+    let p1y = fromIntegral $ y
+    let p2x = fromIntegral $ n * unitWidth + x
+    let p2y = fromIntegral $ y + unitHeight
+    if firstTime then do
+        putStr "(x, y) = "
+        putStr $ show (x, y)
+        putStr ": "
+        putStr "point "
+        putStr $ show (p1x, p1y)
+        putStr " to "
+        putStr $ show (p2x, p2y)
+        putChar '\n'
+    else
+        return ()
+    drawLine renderer (P (V2 p1x p1y)) (P (V2 p2x p2y))
+    drawNodes firstTime (t:otherLines) (x + n * unitWidth + unitWidth) y (unitWidth, unitHeight) drawText renderer
+drawNodes firstTime (((n, VisibleRight):t):otherLines) x y (unitWidth, unitHeight) drawText renderer = do
+    let p1x = fromIntegral $ n * unitWidth + x
+    let p1y = fromIntegral $ y
+    let p2x = fromIntegral $ n * unitWidth + x + unitWidth
+    let p2y = fromIntegral $ y + unitHeight
+    if firstTime then do
+        print "point "
+        print (p1x, p1y)
+        putStr " to "
+        print (p2x, p2y)
+        putChar '\n'
+    else
+        return ()
+    drawLine renderer (P (V2 p1x p1y)) (P (V2 p2x p2y))
+    drawNodes firstTime (t:otherLines) (x + n * unitWidth + unitWidth) y (unitWidth, unitHeight) drawText renderer
+drawNodes firstTime (((n, _):t):otherLines) x y (unitWidth, unitHeight) drawText renderer = drawNodes firstTime (t:otherLines) (x + n * unitWidth + unitWidth) y (unitWidth, unitHeight) drawText renderer
+
+drawTree :: Bool -> (Text -> IO Surface) -> Renderer -> IO ()
+drawTree firstTime drawText renderer = do
+    let demo = Node 2 (Node 1 Leaf Leaf) Leaf
+    when firstTime $ printTree demo
+    drawNodes firstTime (getLines demo) 0 0 (32, 32) drawText renderer
+    -- surface <- drawText $ pack $ printf "Hello, world!"
+    -- texture <- createTextureFromSurface renderer surface
+    -- let rect = Just $ Rectangle (P (V2 0 96)) (V2 30 192)
+    -- copy renderer texture rect Nothing
